c0c287eba6bd7689f75e3139a7074d22cb817315
orlovse <hajoff@gmail.com>
2020-09-06 12:03:29 +0300

    add Stripe payment to server

---
+++ server/src/lib/api/Stripe.ts
@@ -0,0 +1,14 @@
+import stripe from "stripe";
+
+const client = new stripe(`${process.env.S_SECRET_KEY}`, {});
+
+export const Stripe = {
+    connect: async (code: string) => {
+        const response = await client.oauth.token({
+            grant_type: "authorization_code",
+            code,
+        });
+
+        return response;
+    }
+}
