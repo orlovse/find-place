e1fa37949c89aec76d1c8dc339e6676af35000d2
orlovse <hajoff@gmail.com>
2020-09-07 12:49:06 +0300

    add hostListing mutation to server

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -2,7 +2,7 @@ import { IResolvers } from "apollo-server-express";
 import { Request } from "express";
 import { ObjectId } from "mongodb";
 import { Google } from "../../../lib/api";
-import { Database ,Listing, User } from "../../../lib/types";
+import { Database ,Listing, User, ListingType } from "../../../lib/types";
 import { authorize } from "../../../lib/utils";
 import { 
     ListingArgs, 
@@ -11,8 +11,25 @@ import {
     ListingsArgs, 
     ListingsData, 
     ListingsFilter,
-    ListingsQuery
+    ListingsQuery,
+    HostListingInput,
+    HostListingArgs
 } from "./types";
+
+const verifyHostListingInput = ({title, description, type, price}: HostListingInput) => {
+    if (title.length > 100) {
+        throw new Error("listing title must be under 100 characters");
+    }
+    if (description.length > 5000) {
+        throw new Error("listing description must be under 5000 characters");
+    }
+    if(type !== ListingType.Apartment && type !== ListingType.House) {
+        throw new Error("listing type must be either an apartment or house");
+    }
+    if(price < 0) {
+        throw new Error("price must be greater than 0");
+    }
+}
 
 export const listingResolvers: IResolvers = {
     Query: {
@@ -90,6 +107,48 @@ export const listingResolvers: IResolvers = {
         }
 
     },
+
+    Mutation: {
+        hostListing: async (
+            _root: undefined, 
+            { input }: HostListingArgs, 
+            { db, req }: { db: Database, req: Request }
+        ): Promise<Listing> => {
+            verifyHostListingInput(input);
+            let viewer = await authorize(db, req);
+            if (!viewer) {
+                throw new Error("viewer cannot be found");
+            }
+
+            const { country, admin, city } = await Google.geocode(input.address);
+            if (!country || !admin || !city) {
+                throw new Error("invalid address input");
+            }
+
+            const insertResult = await db.listings.insertOne({
+                _id: new ObjectId(),
+                ...input,
+                bookings: [],
+                bookingsIndex: {},
+                country,
+                admin,
+                city,
+                host: viewer._id
+            });
+
+            insertResult.ops[0];
+
+            const insertedListing: Listing = insertResult.ops[0];
+
+            await db.users.updateOne(
+                { _id: viewer._id },
+                { $push: { listings: insertedListing._id }}
+            );
+
+            return insertedListing;
+        }
+    },
+
     Listing: {
         id: (listing: Listing): string => {
             return listing._id.toString();
