4c0ad21a9fc73d0f320635bc197ce43eb68aac2d
orlovse <hajoff@gmail.com>
2020-09-08 13:52:17 +0300

    add host page

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
-import {Home, Host, Listing, Listings, Login, NotFound, Stripe, User } from '../../pages';
+import {
+  Home, 
+  Host, 
+  Listing, 
+  Listings, 
+  Login, 
+  NotFound, 
+  Stripe, 
+  User 
+} from '../../pages';
 import { AppHeader } from "../AppHeader";
 import { Viewer } from "../../lib/types";
 import { LOG_IN } from "../../lib/graphql/mutations";
@@ -63,7 +72,7 @@ export const App = () => {
             </Affix>
             <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/host" component={Host} />
+            <Route exact path="/host" render={props => <Host {...props} viewer={viewer} /> } />
             <Route exact path="/listing/:id" component={Listing} />
             <Route exact path="/listings/:location?" component={Listings} />
             <Route exact path="/login" render={props => <Login {...props} setViewer={setViewer} />} />
